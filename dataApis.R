6ceb075c92c129a4aa30e4f0cda5f3462503ce92
Rodrigo Aldrette <roryaldrettes@hotmail.com>
2025-06-23 21:26:58 -0600

    Agregar apis en R

---
+++ dataApis.R
@@ -0,0 +1,46 @@
+
+
+# ______________________________________________________________________________
+#
+# Proyecto:       API's para análisis de datos
+#                
+#
+# Autor:          Rodrigo Aldrette
+# Email:          raaldrettes@colmex.mx
+#
+# Fecha:          16/02/2025
+#
+# ______________________________________________________________________________
+
+
+
+
+  # Paquetes
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr, jsonlite, httr, purr)
+
+
+font_add_google("Montserrat")
+showtext_auto()
+
+
+# Script _________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
